d501f1607ca0b8e170dcca55cd32acf55a1d7e8e
koutaro harada <haradaktr@intelilink.co.jp>
2024-04-22 18:01:28 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    
    Initial commit
    
    Changes to be committed:
    	new file:   .devcontainer/Dockerfile
    	new file:   .devcontainer/devcontainer.json
    	new file:   .devcontainer/docker-compose.yml
    	new file:   .dockerignore
    	new file:   .funcignore
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/settings.json
    	new file:   .vscode/tasks.json
    	new file:   Dockerfile
    	new file:   function_app.py
    	new file:   host.json
    	new file:   requirements.txt

---
+++ function_app.py
@@ -0,0 +1,63 @@
+import azure.functions as func
+import logging
+import subprocess
+import json
+import re
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route='dotnetversion', methods=['GET'])
+def dotnetversion(req: func.HttpRequest) -> func.HttpResponse:
+   logging.info('dotnetinfo function processed a request.')
+
+   command_to_run = ['dotnet', '--version']
+   result = subprocess.run(command_to_run, capture_output=True, text=True).stdout
+   response_data = {
+      'result': 'success',
+      'version': f'{result.rstrip()}'
+   }
+   response_json = json.dumps(response_data)
+
+   return func.HttpResponse(response_json, status_code=200, mimetype='application/json')
+
+@app.route(route='pacversion', methods=['GET'])
+def pacversion(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('pacversion function processed a request.')
+
+    command_to_run = ['pac']
+    result = subprocess.run(command_to_run, capture_output=True, text=True).stdout
+    match = re.search(r'Version: (.*)', result)
+    response_data = {
+      'result': 'success',
+      'version': f'{match.group(1)}'
+    }
+    response_json = json.dumps(response_data)
+
+    return func.HttpResponse(response_json, status_code=200, mimetype='application/json')
+
+@app.route(route='pacauthtest', methods=['POST'])
+def pacauthtest(req: func.HttpRequest) -> func.HttpResponse:
+   logging.info('pacauthtest function processed a request.')
+
+   req_body = req.get_json()
+   applicationId = req_body.get('applicationId')
+   clientSecret  = req_body.get('clientSecret')
+   tenant = req_body.get('tenant')
+   
+   command_to_run = ['pac', 'auth', 'create', '--name', 'MyOrg-SPN', '--applicationId', applicationId, '--clientSecret', clientSecret, '--tenant', tenant]
+   result = subprocess.run(command_to_run, capture_output=True, text=True)
+   if result.returncode == 0:
+      response_data = {
+         'result': 'success'
+      }
+      response_json = json.dumps(response_data)
+
+      return func.HttpResponse(response_json, status_code=200, mimetype='application/json')
+
+   else:
+      response_data = {
+         'result': 'failed'
+      }
+      response_json = json.dumps(response_data)
+
+      return func.HttpResponse(response_json, status_code=500, mimetype='application/json')
